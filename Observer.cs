d64d3350245f4c3edcac705fb552a9de5e766a18
Sasha Lukyanova <78989954+SashaLukyanova@users.noreply.github.com>
2021-08-27 13:02:36 +0300

    Add files via upload

---
+++ Observer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Patterns
+{
+    public interface IObserver
+    {
+        void Updater(ISubject subject);
+    }
+
+    public interface ISubject
+    {
+        void Attach(IObserver observer);
+        void Detach(IObserver observer);
+        void Notify();
+    }
+
+    public class Subject : ISubject
+    {
+        public int State { get; set; }
+
+        private List<IObserver> _observers = new List<IObserver>();
+
+        public void Attach(IObserver observer)
+        {
+            Console.WriteLine("Subject: Attached an observer.");
+            this._observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            this._observers.Remove(observer);
+            Console.WriteLine("Subject: Detached an observer.");
+        }
+
+        public void Notify()
+        {
+            Console.WriteLine("Subject: Notifying observers...");
+            foreach (var observer in _observers)
+            {
+                observer.Updater(this);
+            }
+        }
+
+        public void SomeBusinessLogic()
+        {
+            Console.WriteLine("\nSubject: I'm doing something important.");
+            this.State = new Random().Next(0, 10);
+
+            Thread.Sleep(15);
+
+            Console.WriteLine("Subject: My state has just changed to:." + this.State);
+            this.Notify();
+
+        }
+
+    }
+
+    class ConcreteObserverA : IObserver
+    {
+        public void Updater(ISubject subject)
+        {
+            if ((subject as Subject).State < 3)
+                Console.WriteLine("ConcreteObserverA: Reacted to the event.");
+        }
+    }
+
+    class ConcreteObserverB : IObserver
+    {
+        public void Updater(ISubject subject)
+        {
+            if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
+                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Patterns
+{   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = new Subject();
+            subject.State = 0;
+
+            var observerA = new ConcreteObserverA();
+            subject.Attach(observerA);
+
+            var observerB = new ConcreteObserverB();
+            subject.Attach(observerB);
+
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
+
+            subject.Detach(observerB);
+
+            subject.SomeBusinessLogic();
+
+        }
+    }
+}
